08bedb6fd15d7e9c9e462405d21a6ecb043438b0
BiancaBaccin <bianca.baccin@gmail.com>
2026-01-29 22:01:40 -0300

    Adiciona PATH para updateUserProfile e updateUserAvatar

--- controllers/users.js
+++ controllers/users.js
@@ -35,3 +35,43 @@ module.exports.createUser = (req, res) => {
       res.status(500).json({ message: "Ocorreu um erro no servidor" });
     });
 };
+
+module.exports.updateUserProfile = (req, res) => {
+  const { name, about } = req.body;
+  const userId = req.user._id;
+
+  User.findByIdAndUpdate(
+    userId,
+    { name, about },
+    { new: true, runValidators: true }
+  )
+    .orFail()
+    .then((user) => res.json(user))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        return res.status(400).json({ message: "Dados inválidos" });
+      }
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(404).json({ message: "Usuário não encontrado" });
+      }
+      res.status(500).json({ message: "Ocorreu um erro no servidor" });
+    });
+};
+
+module.exports.updateUserAvatar = (req, res) => {
+  const { avatar } = req.body;
+  const userId = req.user._id;
+
+  User.findByIdAndUpdate(userId, { avatar }, { new: true, runValidators: true })
+    .orFail()
+    .then((user) => res.json(user))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        return res.status(400).json({ message: "Dados inválidos" });
+      }
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(404).json({ message: "Usuário não encontrado" });
+      }
+      res.status(500).json({ message: "Ocorreu um erro no servidor" });
+    });
+};
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,7 @@ const { getUsers, getUserById, createUser } = require("../controllers/users");
 router.get("/", getUsers);
 router.get("/:userId", getUserById);
 router.post("/", createUser);
+router.patch("/users/me", updateUserProfile);
+router.patch("/users/me/avatar", updateUserAvatar);
 
 module.exports = router;
