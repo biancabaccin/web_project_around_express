f53531073533a2377d385850387d04e1ce62e0f3
BiancaBaccin <bianca.baccin@gmail.com>
2026-01-29 22:51:17 -0300

    Adiciona PUT e DELETE para likeCard e dislikeCard

--- controllers/cards.js
+++ controllers/cards.js
@@ -37,3 +37,33 @@ module.exports.deleteCard = (req, res) => {
       res.status(500).json({ message: "Ocorreu um erro no servidor" });
     });
 };
+
+module.exports.likeCard = (req, res) =>
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true }
+  )
+    .orFail()
+    .then((card) => res.json(card))
+    .catch((err) => {
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(404).json({ message: "Cartão não encontrado" });
+      }
+      res.status(500).json({ message: "Ocorreu um erro no servidor" });
+    });
+
+module.exports.dislikeCard = (req, res) =>
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true }
+  )
+    .orFail()
+    .then((card) => res.json(card))
+    .catch((err) => {
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(404).json({ message: "Cartão não encontrado" });
+      }
+      res.status(500).json({ message: "Ocorreu um erro no servidor" });
+    });
